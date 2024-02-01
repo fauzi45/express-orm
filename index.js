18eaf29b803cd4d09ff7508130e3817483dd5f1a
fauzi45 <uzi.fajar45@gmail.com>
2024-02-01 10:34:39 +0700

    init sequelize department

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const department = require("./server/api/department");
+
+app.use("/department", department);
+
 const port = process.env.NODEJS_PORT || 8000;
 
 app.get("/", (req, res) => {
